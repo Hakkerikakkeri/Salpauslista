fc52e8c3ea75e7a8ad904a76bfc4f3a78916ffd5
erno <91182619+BestoEpe@users.noreply.github.com>
2024-03-12 10:21:44 +0000

    Add files via upload

--- salpauslista/src/components/TheWelcome.vue
+++ salpauslista/src/components/TheWelcome.vue
@@ -1,25 +1,75 @@
-<script setup lang="ts">
-import { ref, onMounted } from 'vue';
-
-const data = ref(null);
-
-onMounted(async () => {
-  try {
-    const response = await fetch('https://api.allorigins.win/get?url=' + encodeURIComponent('http://api.vipunen.fi/api/resources/julkaisut'));
-    if (!response.ok) {
-      throw new Error('Failed to fetch data');
-    }
-    const responseData = await response.json();
-    data.value = JSON.parse(responseData.contents);
-  } catch (error) {
-    console.error('Error fetching data:', error);
-  }
-});
-</script>
-
 <template>
-  <div>
-    <h1 class="title">Data fetched:</h1>
-    <pre class="data">{{ data }}</pre>
+  <div class="container">
+    <h1 class="title">Data:</h1>
+    <div v-if="jsonData" class="data-container">
+      <div v-for="(yearData, year) in jsonData['Kohde 1']" :key="year">
+        <div class="year-header" @click="toggleYear(year)">
+          <span>{{ yearData.vuosi}}</span>
+          <span v-if="isYearOpen(year)">▼</span>
+          <span v-else>►</span>
+        </div>
+        <div v-if="isYearOpen(year)" class="data-list">
+          <div v-for="(value, key) in yearData" :key="key" v-if="key !== 'text'">
+            {{ key }}: {{ value }}
+          </div>
+        </div>
+      </div>
+    </div>
+    <p v-else>No data available</p>
   </div>
 </template>
+
+
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import jsonData from '../data/tilastovuosi.json';
+
+const openYear = ref(null);
+
+function toggleYear(year) {
+  openYear.value = openYear.value === year ? null : year;
+}
+
+function isYearOpen(year) {
+  return openYear.value === year;
+}
+</script>
+
+<style scoped>
+.container {
+  max-width: 800px;
+  margin: auto;
+  padding: 20px;
+  color: black;
+}
+
+.title {
+  font-size: 24px;
+  margin-bottom: 10px;
+}
+
+.data-container {
+  border: 1px solid #ccc;
+  padding: 10px;
+  overflow-x: auto;
+}
+
+.year-header {
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+}
+
+.data-list {
+  margin-left: 20px;
+}
+
+.year-header span {
+  margin-right: 5px;
+}
+
+.year-header:hover {
+  background-color: #f2f2f2;
+}
+</style>
