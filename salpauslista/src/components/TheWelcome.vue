34ff86777ac1f09e9fb56a85cb1668e80b88f2cb
erno <91182619+BestoEpe@users.noreply.github.com>
2024-03-12 10:52:17 +0000

    Update TheWelcome.vue

--- salpauslista/src/components/TheWelcome.vue
+++ salpauslista/src/components/TheWelcome.vue
@@ -2,37 +2,36 @@
   <div class="container">
     <h1 class="title">Data:</h1>
     <div v-if="jsonData" class="data-container">
-      <div v-for="(yearData, year) in jsonData['Kohde 1']" :key="year">
-        <div class="year-header" @click="toggleYear(year)">
-          <span v-if="isYearOpen(year)">▼</span>
-          <span v-else>►</span>
-          <span>{{ yearData.vuosi}}</span>
+      <div class="excel-table">
+        <div class="excel-row header-row">
+          <div class="excel-cell" v-for="(header, index) in headers" :key="index">{{ header }}</div>
         </div>
-        <div v-if="isYearOpen(year)" class="data-list">
-          <div v-for="(value, key) in yearData" :key="key" v-if="key !== 'text'">
-            {{ key }}: {{ value }}
+        <template v-for="(rowData, index) in jsonData['Kohde 1'].slice(1)" :key="index">
+          <div class="excel-row">
+            <div class="excel-cell" v-for="(value, key) in rowData" :key="key">{{ value }}</div>
+            <div class="excel-cell">
+            </div>
           </div>
-        </div>
+          <div v-if="openYears[index]" class="extra-data">
+            <!-- Additional data for the opened year -->
+          </div>
+        </template>
       </div>
     </div>
     <p v-else>No data available</p>
   </div>
 </template>
 
-
-
 <script setup lang="ts">
 import { ref } from 'vue';
 import jsonData from '../data/tilastovuosi.json';
 
-const openYear = ref(null);
+const headers = Object.values(jsonData['Kohde 1'][0]); // Extract headers from the first object
 
-function toggleYear(year) {
-  openYear.value = openYear.value === year ? null : year;
-}
+const openYears = ref(Array(jsonData['Kohde 1'].length - 1).fill(false));
 
-function isYearOpen(year) {
-  return openYear.value === year;
+function toggleYear(index) {
+  openYears.value[index] = !openYears.value[index];
 }
 </script>
 
@@ -50,26 +49,42 @@ function isYearOpen(year) {
 }
 
 .data-container {
-  border: 1px solid #ccc;
-  padding: 10px;
-  overflow-x: auto;
+  overflow: auto;
 }
 
-.year-header {
-  cursor: pointer;
-  display: flex;
-  align-items: center;
+.excel-table {
+  display: table;
+  
+
 }
 
-.data-list {
-  margin-left: 20px;
+.excel-row {
+  display: table-row;
 }
 
-.year-header span {
-  margin-right: 5px;
+.excel-cell {
+  display: table-cell;
+  padding: 8px;
 }
 
-.year-header:hover {
+.header-row {
+  font-weight: bold;
   background-color: #f2f2f2;
+}
+
+.excel-cell:first-child {
+  min-width: 100px; /* Adjust the width of the first column if needed */
+}
+
+.toggle-button {
+  width: 20px;
+  height: 20px;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+
+.extra-data {
+  padding-left: 30px; /* Adjust indentation for the extra data */
 }
 </style>
