2d055a2ea75bcb0326ce2ca1d5399c59273a679f
erno <91182619+BestoEpe@users.noreply.github.com>
2024-03-13 10:33:36 +0000

    Add files via upload

--- salpauslista/src/components/TheWelcome.vue
+++ salpauslista/src/components/TheWelcome.vue
@@ -7,19 +7,11 @@
           <div class="excel-cell" v-for="(header, index) in headers" :key="index">{{ header }}</div>
         </div>
         <template v-for="(rowData, index) in jsonData['Kohde 1']" :key="index">
-          <div class="excel-row" :class="{ 'year-row': isYearRow(rowData) }">
-            <div class="toggle-cell" v-if="isYearRow(rowData)">
-              <button class="toggle-button" @click="toggleYear(index)">
-                {{ openYears.includes(index) && '+ ' }}
-              </button>
-            </div>
-            <div class="excel-cell" v-for="(value, key) in rowData" :key="key">{{ value }}</div>
+          <div class="excel-row">
+            <div v-if="index !== 0" class="excel-cell" v-for="(value, key) in rowData" :key="key">{{ value }}</div>
           </div>
-          <div v-if="openYears.includes(index) && !isYearRow(rowData) && index !== 0" class="extra-data">
-            <div class="excel-row" v-for="(value, key) in extraData[index]" :key="key">
-              <div class="excel-cell">{{ key }}</div>
-              <div class="excel-cell">{{ value }}</div>
-            </div>
+          <div v-if="openYears[index] && index !== 0" class="extra-data">
+            <!-- Additional data for the opened year -->
           </div>
         </template>
       </div>
@@ -29,40 +21,16 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 import jsonData from '../data/tilastovuosi2.json';
 
-const headers = Object.keys(jsonData['Kohde 1'][0]); // Extract headers from the first object
+const headers = Object.values(jsonData['Kohde 1'][0]); // Extract headers from the first object
 
-const openYears = ref([]);
-const extraData = ref(Array(jsonData['Kohde 1'].length).fill({}));
+const openYears = ref(Array(jsonData['Kohde 1'].length).fill(false));
 
 function toggleYear(index) {
-  if (!openYears.value.includes(index)) {
-    openYears.value.push(index);
-    // Populate extra data when opening
-    extraData.value[index] = jsonData['Kohde 1'][index];
-  } else {
-    // Close the year if already open
-    const yearIndex = openYears.value.indexOf(index);
-    if (yearIndex !== -1) {
-      openYears.value.splice(yearIndex, 1);
-    }
-  }
+  openYears.value[index] = !openYears.value[index];
 }
-
-function isYearRow(rowData) {
-  return rowData.hasOwnProperty('vuosiluku');
-}
-
-onMounted(() => {
-  // Populate openYears with the indices of the year rows initially
-  jsonData['Kohde 1'].forEach((rowData, index) => {
-    if (isYearRow(rowData)) {
-      openYears.value.push(index);
-    }
-  });
-});
 </script>
 
 <style scoped>
@@ -122,9 +90,5 @@ onMounted(() => {
 
 .extra-data {
   padding-left: 30px; /* Adjust indentation for the extra data */
-}
-
-.year-row {
-  font-weight: bold;
 }
 </style>
