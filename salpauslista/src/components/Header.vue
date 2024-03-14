92e28b88450b184d322b3a6534dddc184989fc00
Hakkerikakkeri <88773115+Hakkerikakkeri@users.noreply.github.com>
2024-03-14 10:44:19 +0200

    Sticky header

--- salpauslista/src/components/Header.vue
+++ salpauslista/src/components/Header.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue';
 
+const isSticky = ref(false);
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll);
+});
+
+const handleScroll = () => {
+  if (window.pageYOffset > 0) {
+    isSticky.value = true;
+  } else {
+    isSticky.value = false;
+  }
+};
 </script>
 
 <template>
-  <div class="header">
+  <div :class="{ 'header': true, 'sticky': isSticky }">
     <img class="logo" src="https://kuljettajaopetus.fi/dokumentit/asiakkaat/salpaus/kuvat/logo_iso.png">
     <input class="searchbar" placeholder="Hae kentästä">
     <button class="excel-button">Lataa excelinä</button>
